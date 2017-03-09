f8454a9caa8ac120604a6e79dcc915e7a5aeb590
eudy1990p <eudy1990@gmail.com>
2017-03-09 11:20:39 -0400

    actualizar

--- src/RecursoSql/db.sql
+++ src/RecursoSql/db.sql
@@ -25,7 +25,7 @@ create table usuario(
     usuario_id int 
 );
 insert into usuario 
-(nombre_usuario, clave_usuario, fecha_creado,usuario_id,type_of_user) 
+(nombre_usuario, clave_usuario, fecha_creado,usuario_id,tipo_usuario) 
 values
 ('vyr','vyr123456@',now(),'1','admin');
 
@@ -42,7 +42,7 @@ create table persona(
     fecha_creado datetime,
     usuario_id int,
     foreign key (usuario_id) references usuario(id),
-    display int(1) default 1
+    display int(1) default '1'
 );
 
 create table tipo_telefono(
@@ -51,7 +51,7 @@ create table tipo_telefono(
     fecha_creado datetime,
     usuario_id int,
     foreign key (usuario_id) references usuario(id),
-    display int(1) default 1
+    display int(1) default '1'
 );
 insert into tipo_telefono 
 (nombre, fecha_creado,usuario_id) 
@@ -69,7 +69,7 @@ create table telephone(
     foreign key (usuario_id) references usuario(id),
     foreign key (persona_id) references persona(id),
     foreign key (tipo_telefono_id) references tipo_telefono(id),
-    display int(1) default 1
+    display int(1) default '1'
 );
 
 create table email(
@@ -78,9 +78,9 @@ create table email(
     fecha_creado datetime,
     persona_id int,
     usuario_id int,
-    foreign key (usuario_id) references u(id),
+    foreign key (usuario_id) references usuario(id),
     foreign key (persona_id) references persona(id),
-    display int(1) default 1
+    display int(1) default '1'
 );
 
 create table direccion(
@@ -93,13 +93,13 @@ create table direccion(
     usuario_id int,
     foreign key (usuario_id) references usuario(id),
     foreign key (persona_id) references persona(id),
-    display int(1) default 1
+    display int(1) default '1'
 );
 
 /*
     EMPLEADO
 */ 
-create table empleado (){
+create table empleado (
     id int not null primary key auto_increment,
     persona_id int,
     fecha_creado datetime,
@@ -108,29 +108,29 @@ create table empleado (){
     foreign key (usuario_id) references usuario(id),
     foreign key (usuario_empleado_id) references usuario(id),
     foreign key (persona_id) references persona(id),
-    display int(1) default 1
-}
+    display int(1) default '1'
+);
 
 /*
     CLIENTE
 */ 
-create table cliente (){
+create table cliente (
     id int not null primary key auto_increment,
     persona_id int,
     fecha_creado datetime,
     usuario_id int,
     foreign key (usuario_id) references usuario(id),
     foreign key (persona_id) references persona(id),
-    display int(1) default 1
-}
-create table nota(){
+    display int(1) default '1'
+);
+create table nota_cliente(
     id int not null primary key auto_increment,
     cliente_id int,
     usuario_id int,
     foreign key (usuario_id) references usuario(id),
     foreign key (cliente_id) references cliente(id),
-    display int(1) default 1
-}
+    display int(1) default '1'
+);
 
 
 /*
@@ -141,24 +141,216 @@ create table nota(){
      y se factura y se reduce la cantidad
    
 */
-create table producto_inventariado(){
+create table producto_inventariado(
     id int not null primary key auto_increment,
     usuario_id int,
-    nombre unique varchar(250),
+    nombre varchar(250) unique,
     cantidad_comprada int,
     precio_compra decimal(20,2),
     precio_venta decimal(20,2),
     foreign key (usuario_id) references usuario(id),
-    display int(1) default 1
-}
+    display int(1) default '1'
+);
+
+
 
 /*
-    
-    no repetir
+    FACTURA
 */
-create table auto_completar(){
+
+create table factura(
     id int not null primary key auto_increment,
-    nombre varchar(250) unique,
-    precio_venta decimal(20,2),
+    usuario_id int,
+    cuadrada int(1) default '0',
+    sub_total decimal(20,2),
+    itbis decimal(20,2),
+    tiene_itbis int(1) default '1',
+    total decimal(20,2),
+    numero_comprovante_fiscal varchar(50),
+    fecha_creada datetime,
+    cliente_id int,
+    estado enum('anulada','saldada','pendiente'),
+    nota text,
+    porciente_itbis int,
+    tiene_comprovante_fiscal int(1) default '0',
+    balance_deuda decimal(20,2),
+    monto_pagado decimal(20,2),
+    cambio_efectivo  decimal(20,2),
+    usuario_id_anulado int,
+    comentario_anulado text,
+    foreign key (usuario_id) references usuario(id),
+    foreign key (usuario_id_anulado) references usuario(id),
+    foreign key (cliente_id) references cliente(id),
     display int(1) default 1
-}
+);
+
+create table factura_detalle(
+    id int not null primary key auto_increment,
+    usuario_id int,
+    nombre varchar(200),
+    precio decimal(20,2),
+    cantidad int,
+    total decimal(20,2),
+    fecha_creada datetime,
+    producto_inventariado_id int,
+    cliente_id int,
+    estado enum('anulada','pendiente') default 'pendiente',    
+    usuario_id_anulado int,
+    comentario_anulado text,
+    foreign key (usuario_id) references usuario(id),
+    foreign key (producto_inventariado_id) references producto_inventariado(id),
+    foreign key (usuario_id_anulado) references usuario(id),
+    display int(1) default 1
+);
+
+/*
+    COTIZACION
+*/
+
+create table cotizacion(
+    id int not null primary key auto_increment,
+    usuario_id int,
+    sub_total decimal(20,2),
+    itbis decimal(20,2),
+    tiene_itbis int(1) default 1,
+    total decimal(20,2),
+    fecha_creada datetime,
+    cliente_id int,
+    estado enum('anulada','facturada','pendiente'),
+    nota text,
+    porciente_itbis int,
+    usuario_id_anulado int,
+    comentario_anulado text,
+    foreign key (usuario_id) references usuario(id),
+    foreign key (usuario_id_anulado) references usuario(id),
+    foreign key (cliente_id) references cliente(id),
+    display int(1) default 1
+);
+
+create table cotizacion_detalle(
+    id int not null primary key auto_increment,
+    usuario_id int,
+    nombre varchar(200),
+    precio decimal(20,2),
+    cantidad int,
+    total decimal(20,2),
+    fecha_creada datetime,
+    producto_inventariado_id int,
+    cliente_id int,
+    estado enum('anulada','pendiente') default 'pendiente',    
+    usuario_id_anulado int,
+    comentario_anulado text,
+    foreign key (usuario_id) references usuario(id),
+    foreign key (producto_inventariado_id) references producto_inventariado(id),
+    foreign key (usuario_id_anulado) references usuario(id),
+    display int(1) default 1
+);
+
+/*
+    CUADRE DE CAJA
+*/
+create table cuadre(
+    id int not null primary key auto_increment,
+    usuario_id int,
+    fecha_creado datetime,
+    fecha_desde date,
+    fecha_hasta date,
+    monto_facturado decimal(20,2),
+    monto_en_caja decimal(20,2),
+    foreign key (usuario_id) references usuario(id),
+    display int(1) default 1
+);
+create table cuadre_detalle(
+    id int not null primary key auto_increment,
+    usuario_id int,
+    fecha_creado datetime,
+    factura_id int,
+    foreign key (factura_id) references factura(id),
+    foreign key (usuario_id) references usuario(id),
+    display int(1) default 1
+);
+
+/*
+    PAGOS
+*/
+create table pagos(
+    id int not null primary key auto_increment,
+    usuario_id int,
+    fecha_creado datetime,
+    monto_pagar decimal(20,2),
+    balance_anterior decimal(20,2),
+    balance_despues_del_pago decimal(20,2),
+    cambio decimal(20,2),
+    tipo_pago enum('efectivo','cheque','tarjeta','transferencia'),
+    factura_id int,
+    foreign key (factura_id) references factura(id),
+    foreign key (usuario_id) references usuario(id),
+    display int(1) default 1
+);
+
+/*
+    NOMINA DE PAGOS
+*/
+create table pago_nomina_empleado(
+    id int not null primary key auto_increment,
+    usuario_id int,
+    fecha_creado datetime,
+    monto_pagar_al_empleado decimal(20,2),
+    foreign key (usuario_id) references usuario(id),
+    display int(1) default 1
+);
+
+/*
+    REPARACION
+*/
+create table inventario_reparaciones(
+    id int not null primary key auto_increment,
+    usuario_id int,
+    nombre varchar(250) unique,
+    cantidad_compada int,
+    precio_compra decimal(20,2),
+    precio_venta decimal(20,2),
+    foreign key (usuario_id) references usuario(id),
+    display int(1) default 1
+);
+
+create table reparacion(
+    id int not null primary key auto_increment,
+    usuario_id int,
+    sub_total decimal(20,2),
+    fecha_creada datetime,
+    cliente_id int,
+    estado enum('anulada','procesando','pendiente','completo'),
+    nota text,
+    usuario_id_anulado int,
+    comentario_anulado text,
+    foreign key (usuario_id) references usuario(id),
+    foreign key (usuario_id_anulado) references usuario(id),
+    foreign key (cliente_id) references cliente(id),
+    display int(1) default 1
+);
+
+create table reparacion_detalle(
+    id int not null primary key auto_increment,
+    usuario_id int,
+    nombre varchar(200),
+    precio decimal(20,2),
+    precio_completado decimal(20,2),
+    cantidad int,
+    total decimal(20,2),
+    fecha_creada datetime,
+    inventario_reparaciones_id int,
+    cliente_id int,
+    estado enum('anulada','pendiente','completado','procesando','incompleto') default 'pendiente',
+    nota_incompleto_por text,
+    usuario_id_incompleto_por int,
+    nota text,
+    usuario_id_completado int,
+    usuario_id_anulado int,
+    comentario_anulado text,
+    foreign key (usuario_id) references usuario(id),
+    foreign key (usuario_id_completado) references usuario(id),   
+     foreign key (usuario_id_incompleto_por) references usuario(id),
+    foreign key (inventario_reparaciones_id) references inventario_reparaciones(id),
+    display int(1) default 1
+);
